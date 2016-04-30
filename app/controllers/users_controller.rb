88328259f2df3771a12c2f6aaf7724c11ab4856b
kotaroiyanaga <suisougakubasukura@gmail.com>
2016-04-30 11:02:59 +0000

    challenge1 first commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
   
   def show
     @user = User.find(params[:id])
@@ -6,6 +7,26 @@ class UsersController < ApplicationController
   
   def new
     @user = User.new
+  end
+  
+  def edit
+    if (current_user != params[:id])
+       redirect_to "home"
+    end
+     # http://hogehoge/users/1/edit
+    @user = User.find(params[:id])
+     # form_for(@user) 
+  end
+  
+  def update
+    if (current_user != params[:id])
+       redirect_to "home"
+    end
+    if @user.update(user_params)
+      redirect_to user_path , notice: 'ユーザー情報を更新しました'
+    else
+      render 'edit'
+    end
   end
   
   def create
